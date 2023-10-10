d156c5cd3a33d02c7b286ed326494d334753702a
Younji Woo <younjiwoo@gmail.com>
2023-10-10 17:18:45 +0900

    initial commit

---
+++ src/components/NewTask/NewTask.js
@@ -0,0 +1,49 @@
+import { useState } from 'react';
+
+import Section from '../UI/Section';
+import TaskForm from './TaskForm';
+
+const NewTask = (props) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const enterTaskHandler = async (taskText) => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(
+        'https://react-http-af8a7-default-rtdb.firebaseio.com/tasks.json',
+        {
+          method: 'POST',
+          body: JSON.stringify({ text: taskText }),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error('Request failed!');
+      }
+
+      const data = await response.json();
+
+      const generatedId = data.name; // firebase에서만 해당하는 부분 => "name"은 생성된 id
+      const createdTask = { id: generatedId, text: taskText };
+
+      props.onAddTask(createdTask);
+    } catch (err) {
+      setError(err.message || 'Something went wrong!');
+    }
+    setIsLoading(false);
+  };
+
+  return (
+    <Section>
+      <TaskForm onEnterTask={enterTaskHandler} loading={isLoading} />
+      {error && <p>{error}</p>}
+    </Section>
+  );
+};
+
+export default NewTask;
